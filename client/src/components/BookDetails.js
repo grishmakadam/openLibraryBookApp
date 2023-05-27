beced3ba15b94db5a8c06ec218a2509cb0680574
grishmakadam <grishmakadam@yahoo.com>
2023-05-27 14:46:02 +0530

    book details component modified

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,10 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+const BookDetails = () => {
+const [data,setData]=useState()
+  const {id}=useParams()
 
-const BookDetails = ({}) => {
+  const getData=async()=>{
+    try{
+      const res=await axios.get(`https://openlibrary.org/works/${id}.json`)
+      console.log(res.data)
 
+      setData(res.data.title)
+    }catch(e){
+      console.log(e)
+    }
+  }
+  useEffect(()=>{
+      getData()
+
+  },[])
     
   return (
-    <div>hii</div>
+    <div>{data}</div>
   )
 }
 
