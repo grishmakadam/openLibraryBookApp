26ca064401c0b635d1aaf1c19911f45c35a7ae5f
grishmakadam <grishmakadam@yahoo.com>
2023-06-24 13:54:11 +0530

    Changes added: bookdetails.js modified

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -2,12 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
-import { Card, Container, Grid, Typography } from "@mui/material";
-import {InputLabel} from "@mui/material";
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { InputLabel } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import { Select ,Rating} from "@mui/material";
+import { Select, Rating } from "@mui/material";
 import image from "../image.jpg";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+
 const BookDetails = () => {
   const [data, setData] = useState({});
   const { id } = useParams();
@@ -17,8 +26,8 @@ const BookDetails = () => {
     try {
       const res = await axios.get(`https://openlibrary.org/works/${id}.json`);
       const book = books.filter((x) => x.id.slice(7, x.id.length) == id);
-      console.log(book);
-      setData({ ...book[0] });
+    
+      setData({ ...book[0],description:res.data.description});
       return res.data;
     } catch (e) {
       console.log(e);
@@ -31,67 +40,99 @@ const BookDetails = () => {
   }, []);
 
   return (
-    <Grid container height="100vh" pt={5}>
+    <Grid container p={5}>
       <Grid
         item
-        md={6}
-        
+        md={4}
         alignItems="center"
         display="flex"
         flexDirection="column"
+        justifyContent="center"
+        container
+        spacing={2}
       >
-        <img
-          src={
-            data.cover_id != undefined
-              ? `https://covers.openlibrary.org/b/id/${data.cover_id}-L.jpg`
-              : image
-          }
-          alt={data.title}
-        />
-
-        <FormControl sx={{width:"50%",marginTop:"10px"}}>
-      
-          <Select
-              id="demo-simple-select"
-
-            
-            //value={age}
-            defaultValue={10}
-            sx={{   borderRadius:"40px",
-            '.MuiOutlinedInput-notchedOutline': {
-              borderColor: '#3f8363',
-            },
-            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-              borderColor: '#3f8363',
-            },
-            '&:hover .MuiOutlinedInput-notchedOutline': {
-              borderColor: '#3f8363',
-            },
-            '.MuiSvgIcon-root ': {
-              fill: "white !important",
+        <Grid item>
+          <img
+            src={
+              data.cover_id != undefined
+                ? `https://covers.openlibrary.org/b/id/${data.cover_id}-M.jpg`
+                : image
             }
-          }}
-           
-            // onChange={handleChange}
-          >
-            <MenuItem value={10}>Want to Read</MenuItem>
-            <MenuItem value={20}>Reading</MenuItem>
-            <MenuItem value={30}>Read</MenuItem>
-          </Select>
-        </FormControl>
+            alt={data.title}
+          />
+        </Grid>
+        <Grid item>
+          <FormControl sx={{ width: "50%", marginTop: "10px" }}>
+            <Select
+              id="demo-simple-select"
+              IconComponent={(props) => (
+                <Button
+                  variant="contained"
+                  {...props}
+                  sx={{
+                    height: "100%",
+                    m: "-16px -7px 0 0",
+                    background: "#3f8363",
+                    borderEndStartRadius: 0,
+                    borderStartStartRadius: 0,
+                  }}
+                >
+                  <KeyboardArrowDown
+                    sx={{ fontSize: "30px", color: "white" }}
+                  />
+                </Button>
+              )}
+              //value={age}
+
+              defaultValue={10}
+              sx={{
+                height: "45px",
+                width: "250px",
+                ".MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#3f8363",
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#3f8363",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#3f8363",
+                },
+                ".MuiSvgIcon-root ": {
+                  fill: "white !important",
+                },
+              }}
+
+              // onChange={handleChange}
+            >
+              <MenuItem value={10}>Want to Read</MenuItem>
+              <MenuItem value={20}>Reading</MenuItem>
+              <MenuItem value={30}>Read</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
       </Grid>
-      <Grid item md={6} 
-       
-        display="flex"
-        flexDirection="column">
+      <Grid item md={8} display="flex" flexDirection="column">
         <Typography variant="h3">{data.title}</Typography>
         <Typography variant="h5">{data.author}</Typography>
         <Grid item display="flex">
-        <Rating name="read-only" value={data.ratings_average} precision={0.5} readOnly />
-        <Typography>{data.ratings_average}</Typography>
-        </Grid>
+          <Rating
+            value={data.ratings_average}
       
+           precision={0.5}
+            max={5}
+            name="simple-controlled" 
+            onChange={(event, newValue) => {
+              setData(prev=>({...prev,ratings_average:newValue}))
+            }}
+           readOnly
 
+          />
+          <Typography>{data.ratings_average}</Typography>
+        </Grid>
+
+        <Typography>
+          {data.description}
+        </Typography>
       </Grid>
     </Grid>
   );
